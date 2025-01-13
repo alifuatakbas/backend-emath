d3fdab907c83d98695b13232bcc144a2b6d2537b
Ali Fuat Akbas <alifuatakbas-3@hotmail.com>
2025-01-13 06:11:14 +0300

    reundo table

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,7 @@ class UserDB(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), unique=True, index=True)  # 100 karakter uzunluğunda
     email = Column(String(100), unique=True, index=True)  # 100 karakter uzunluğunda
     full_name = Column(String(100))  # 100 karakter uzunluğunda
     hashed_password = Column(String(100))  # 100 karakter uzunluğunda
