b9ecae9cc6013fb7adcd71de33de3f4c02448815
Ali Fuat Akbas <alifuatakbas-3@hotmail.com>
2025-01-13 06:14:41 +0300

    nt

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.include_router(exams.router)
 # CORS ayarları
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://e-math-frontend.vercel.app/"],
+    allow_origins=["https://e-math-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
