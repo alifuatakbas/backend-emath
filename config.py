4eeaea978f907b63fced9fa5dc9075f1717e8ec1
Ali Fuat Akbas <alifuatakbas-3@hotmail.com>
2025-05-04 17:36:25 +0300

    sınavbasvuru

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
+    SECRET_KEY: str = "gizli_anahtar_buraya"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 210
-
-    class Config:
-        env_file = ".env"
 
 settings = Settings()
